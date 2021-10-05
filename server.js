3cdf3068c7957f209cd9b1be55b0dd84d0f1df9a
aopeacock <aopeacock1@gmail.com>
2021-10-05 14:53:59 -0500

    adding project to github

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+    //   port : 3306,
+      user : 'postgres',
+      password : '1234',
+      database : 'smartbrain'
+    }
+  });
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+db.select('*').from('users').then(data => {
+    console.log('db up');
+  });
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// const database = {
+//     users: [
+//         {
+//             id: '123',
+//             name: 'John',
+//             email: 'john@gmail.com',
+//             password: 'cookies', 
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: '124',
+//             name: 'Sally',
+//             email: 'Sally@gmail.com',
+//             password: 'bananas', 
+//             entries: 0,
+//             joined: new Date()
+
+//         }
+//     ]
+// }
+
+// app.get('/', (req, res) => {
+//     res.send(database.users);
+// })
+
+app.post('/signin', signin.handleSignin( db, bcrypt))
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
+/*
+Planning what our server will need to do
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user 
+
+*/
